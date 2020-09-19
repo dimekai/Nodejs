5fc1c4d504f6a842d89c686bd2c55726601fa232
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-09-19 00:28:23 -0500

    Implementación de funcion crear y guardarDB

---
+++ Project01-ToDo/toDo/toDo.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const colors = require('colors');
+
+// Vamos a guardarlo en un arreglo
+let listToDo = [];
+
+const guardarDB = () => {
+
+    // Convertimos nuestra lista de tareas en un JSON 
+    let data = JSON.stringify(listToDo);
+
+    // Guardamos nuestros datos de forma 
+    // persistente en el archivo data.json 
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err)
+            throw new Error('No se ha podido grabajar', err);
+    });
+}
+
+/**
+ * @param {string} desc 
+ * @returns {string} tarea
+ * @description Se crea una tarea y se almacena en la lista de tareas
+ */
+const crear = (desc) => {
+
+    // Creamos una variable donde se guarde la tarea
+    let tarea = {
+        descripcion: desc,
+        completado: false
+    };
+
+    // Lo agregamos a la lista de tareas
+    listToDo.push(tarea);
+
+    // Guardamos en el archivo JSON
+    guardarDB();
+
+    return tarea;
+}
+
+
+
+module.exports = { crear };
