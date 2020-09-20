3cb6e70721ccbb153cb23fe8e32683696973e212
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-09-19 19:03:56 -0500

    Se ha usado la funcion getClima

--- Project02-WorldClimate/app.js
+++ Project02-WorldClimate/app.js
@@ -1,9 +1,17 @@
 const argv = require('./config/yargs').argv;
 const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
 
 lugar.getLugarLatLong(argv.direccion)
     .then(response => {
         console.log(response);
     }).catch(error => {
         console.log("Hubo un error\n", error);
+    });
+
+clima.getClima(19.41944, -99.14556)
+    .then(resp => {
+        console.log(resp);
+    }).catch(error => {
+        console.log('Hubo un error para el clima', error);
     });
