0bf9f141851726d9930a40da9ee25eaa51f77114
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-10-15 18:07:32 -0500

    Server using Express

---
+++ WebServer/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Usando el middleware
+app.use(express.static(__dirname + '/public'));
+
+/*
+app.get('/', (req, res) => {
+    //res.send('Hola Mundo');
+    let salida = {
+        nombre: "Jesus",
+        edad: 23,
+        url: req.url,
+        sexo: "Masculino"
+    };
+    res.send(salida);
+});
+*/
+
+/*
+app.get('/data', (req, res) => {
+    res.send("Hola Data");
+});
+¨*/
+
+app.listen(PORT, () => {
+    console.log(`Escuchando peticiones en el puerto ${PORT}`);
+});
