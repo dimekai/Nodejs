e20ad5753164433d6adaf6a2160064fbc3098377
JesusDiaz08 <jesus_kdm@hotmail.com>
2020-10-19 10:27:02 -0500

    Server usando el archivo de configuración

--- 04REST Server/server/server.js
+++ 04REST Server/server/server.js
@@ -1,8 +1,10 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT;
 
 // Middlewares
 // parse application/x-www-form-urlencoded
